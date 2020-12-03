bd9a62c9b3345ee01e1dd24b5b906128a8cd39b1
Meredith Coulter <mscoulter@paypal.com>
2020-12-03 10:58:33 -0500

    Update AoC_day_1.py
    created 2 sum algorithm, needs 3 sum algorithm

--- AoC_day_1.py
+++ AoC_day_1.py
@@ -4,8 +4,18 @@ with open('day1inputs.txt') as f:
     for line in f:
         expenses.append(int(line))
 
-minimum = 0
-maximum = len(expenses)-1
+min = 0
+max = len(expenses)-1
 expenses.sort()
 
-print(expenses)
+#Two Sum
+while min < max:
+    sum = expenses[min] + expenses[max]
+    if sum == 2020:
+        print(expenses[min] * expenses[max])
+        break
+
+    if sum > 2020:
+        max -= 1
+    else:
+        min += 1
